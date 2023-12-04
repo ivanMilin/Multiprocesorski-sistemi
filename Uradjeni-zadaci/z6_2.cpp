27391cdc32e7c148c94035956277924ae6bff000
ivanMilin <ivan.milin32@gmail.com>
2023-12-04 01:47:35 +0100

    small changes

--- Uradjeni-zadaci/z6_2.cpp
+++ Uradjeni-zadaci/z6_2.cpp
@@ -1,61 +1,63 @@
-#include <bits/stdc++.h> 
-#include <cmath>
+#include <stdio.h>
+#include <stdlib.h>
 #include <omp.h>
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 
-using namespace std; 
-  
-void storePrimeNumbers(const char *fname,int *prime_numbers, int n){
-    FILE *f = fopen(fname,"w");
-    for(int i = 0; i != n; i++){
-        fprintf(f,"%d ",prime_numbers[i]);
+void savePrimeNumbersToFile(const char *filename, const std::vector<int>& primes) {
+    FILE *f = fopen(filename, "w");
+    if (f == NULL) {
+        printf("Nisam uspeo da otvorim fajl");
+        return;
     }
+
+    for (int i = 0; i < primes.size(); i++) {
+        fprintf(f, "%d ", primes[i]);
+    }
+
     fclose(f);
 }
 
-void SieveOfEratosthenes(int n) { 
-    bool prime[n + 1]; 
-    int prime_numbers[n];
-    memset(prime, true, sizeof(prime)); 
-  
-    #pragma omp parallel for schedule(static,1)
-    for (int p = 2; p <= (int)ceil(sqrt(n)); p++) { 
-         
-        if (prime[p] == true) { 
+void sieveOfEratosthenes(int count, std::vector<int>& prime_numbers) {
+    bool arr[count];
 
-            #pragma omp parallel for schedule(static,1)
-            for (int i = p * p; i <= n; i += p) 
-                prime[i] = false; 
-        } 
-    } 
-   
-    for (int p = 2; p <= n; p++){
-        if (prime[p]){
-            cout << p << " ";
+    memset(arr, true, sizeof(arr));
+
+    #pragma omp parallel for schedule(static, 1)
+        for (int i = 2; i < count; i++) {
+            for (int j = i * i; j < count; j += i) {
+                arr[j - 1] = false;
+            }
         }
-    }        
-    storePrimeNumbers("prime_numbers.txt",prime_numbers,n);
+
+        for (int i = 2; i < count; i++) {
+            if (arr[i - 1] == true)
+                prime_numbers.push_back(i);
+        }
 }
-  
-int main() 
-{ 
-    int number;
-    cout << "Uneti broj do kog je potrebno traziti proste brojeve: ";
-    cin >> number;
-    cout << "\nFollowing are the prime numbers smaller " << " than or equal to " << number << endl; 
-    
+
+int main(int argc, char* argv[]) {
+    int count;
+    std::vector<int> prime_numbers;
+
+    const char* type[5] = {"static","dynamic", "guided", "auto", "runtime"};
+
+    printf("\nBroj do kog je potrebno traziti proste brojeve: ");
+    scanf("%d", &count);
+
     double s = omp_get_wtime();
-    
-    SieveOfEratosthenes(number);
+
+    sieveOfEratosthenes(count, prime_numbers);
+
+    savePrimeNumbersToFile("prime_numbers.txt", prime_numbers);
 
     s = omp_get_wtime() - s;
+    
+    printf("Elapsed time %lf s\n",s);
 
-    cout << "\nElapsed time: "<<s<<" s\n"<<endl;
-    return 0; 
+    return 0;
 }
 
 
-/*
-1. Obrisi brojeve iz fajla pri svakom novom pokretanju
-2. Moras da skontas kako da smestan elemente jedan za drugim a ne da postoji offset izmedju svakog koji je zapravo index prosti broj
 
-*/
